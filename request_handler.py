9d8c49344118b352e4d85dfdbfae0e5d8dc1895e
Alessandro Pina <pina.alessandro17@gmail.com>
2019-11-11 17:52:56 +0100

    Reading article working.

--- request_handler.py
+++ request_handler.py
@@ -7,7 +7,7 @@ def build_response(text_response, url, paragraph):
     return {
         'fulfillmentText': text_response,
         "outputContexts": [{
-            "name": "projects/<Project ID>/agent/sessions/<Session ID>/contexts/open-online",
+            "name": "projects/<Project ID>/agent/sessions/<Session ID>/contexts/web-page",
             "lifespanCount": 1,
             "parameters": {
                 "url": url,
@@ -31,23 +31,23 @@ class RequestHandler:
         context = next((x for x in contexts if "web-page" in x.get("name")), None)
         # Get context parameters from request, if available.
         context_url = context.get("parameters").get("url")
-        context_paragraph = context.get("parameters").get("paragraph")
+        context_paragraph = int(context.get("parameters").get("paragraph"))
 
         if action == "VisitPage":
             return self.visit_page(request)
-        elif action == 'GetTitle':
-            return self.get_title(request)
+        elif action == 'GetInfo':
+            return self.get_info(context_url)
         elif action == 'GetMenu':
             return self.get_menu(context_url)
         elif action == 'ReadPage':
-            return self.read_page(request, context_url, context_paragraph)
+            return self.read_page(context_url, context_paragraph)
 
     def visit_page(self, request):
         # Get url from parameters.
         url = request.get("queryResult").get("parameters").get("url")
         # TODO understand what type of page is.
         try:
-            self.url_parser.process_url(url)
+            self.url_parser.get_soup(url)
             text_response = "%s visited successfully!" % url
         except Exception as ex:
             text_response = ex.args[0]
@@ -60,9 +60,16 @@ class RequestHandler:
         # TODO get elements of menu, how many?
         return build_response(text_response, url=url, paragraph=0)
 
-    def get_title(self, request):
-        return build_response(self.get_title(request), url=context_url, paragraph=context_paragraph)
+    def get_info(self, url):
+        text_response = self.url_parser.get_info(url)
+        return build_response(text_response, url=url, paragraph=0)
 
-    def read_page(self, request, url, paragraph):
+    def read_page(self, url, paragraph):
         # todo if page is article, read it. otherwise read main article titles available.
-        return build_response(self.get_article(request), url=url, paragraph=paragraph)
+        try:
+            text_response = self.url_parser.get_article(url, paragraph)
+            paragraph += 1
+        except IndexError as err:
+            text_response = "End of article."
+            paragraph = 0
+        return build_response(text_response, url=url, paragraph=paragraph)
--- url_parser.py
+++ url_parser.py
@@ -8,7 +8,7 @@ class UrlParser:
     def __init__(self):
         self.http = urllib3.PoolManager(timeout=urllib3.Timeout(connect=5.0, read=5.0))
 
-    def process_url(self, url):
+    def get_soup(self, url):
         try:
             response = self.http.request('GET', url)
         except urllib3.exceptions.MaxRetryError:
@@ -18,14 +18,16 @@ class UrlParser:
             print("Not a valid URL: %s" % url)
             raise Exception("Error while visiting the page.")
 
-        soup = BeautifulSoup(response.data, 'html5lib')
+        soup = BeautifulSoup(response.data, 'html.parser')
         return soup
 
-    def get_title(self, request):
-        return self.soup.title.string
+    def get_info(self, url):
+        soup = self.get_soup(url)
+        # todo get info about the page.
+        return soup.title.string
 
     def get_menu(self, url):
-        soup = self.process_url(url)
+        soup = self.get_soup(url)
         # Get all the single text strings inside ul elements of class menu.
         strings = [ul_menu.text for ul_menu in soup.findAll(name="ul", attrs={'class': 'menu'})]
         strings = [string.split('\n') for string in strings]
@@ -40,6 +42,12 @@ class UrlParser:
         print(joined_strings)
         return joined_strings
 
-    def get_article(self, request):
-        article_url = request.get('queryResult').get('outputContexts')[0].get('parameters').get('page-url')
+    def get_article(self, url, paragraph):
         # TODO recognize if page is a section or an article, behavior changes accordingly.
+        soup = self.get_soup(url)
+        # Find article div.
+        article_div = soup.findAll(name="div", attrs={'class': 'news__content'})[0]
+        # If paragraph is available, read it.
+        div_paragraphs = article_div.findAll('p')
+        string = "%s\n %d paragraph(s) left." % (div_paragraphs[paragraph].text, len(div_paragraphs) - paragraph)
+        return string
