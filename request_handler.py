8853663a5bddacd6ed9f04ec28a9f81326c79627
Alessandro Pina <pina.alessandro17@gmail.com>
2019-11-12 21:18:28 +0100

    WIP navigate website through anchors.

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from url_parser import UrlParser
 
 
-def build_response(text_response, url, paragraph):
+def build_response(text_response, url, paragraph=0, article=0):
     print("URL: %s - Paragraph: %d" % (url, paragraph))
 
     return {
@@ -11,7 +11,8 @@ def build_response(text_response, url, paragraph):
             "lifespanCount": 1,
             "parameters": {
                 "url": url,
-                "paragraph": paragraph
+                "paragraph": paragraph,
+                "article": article
             }
         }]
     }
@@ -31,7 +32,12 @@ class RequestHandler:
         context = next((x for x in contexts if "web-page" in x.get("name")), None)
         # Get context parameters from request, if available.
         context_url = context.get("parameters").get("url")
-        context_paragraph = int(context.get("parameters").get("paragraph"))
+        try:
+            context_paragraph = int(context.get("parameters").get("paragraph"))
+            context_article = int(context.get("parameters").get("article"))
+        except TypeError:
+            context_paragraph = 0
+            context_article = 0
 
         if action == "VisitPage":
             return self.visit_page(request)
@@ -40,36 +46,59 @@ class RequestHandler:
         elif action == 'GetMenu':
             return self.get_menu(context_url)
         elif action == 'ReadPage':
-            return self.read_page(context_url, context_paragraph)
+            return self.read_page(context_url, context_paragraph, context_article)
+        elif action == "GoToSection":
+            return  self.go_to_section(context_url)
+
 
     def visit_page(self, request):
         # Get url from parameters.
         url = request.get("queryResult").get("parameters").get("url")
-        # TODO understand what type of page is.
+
         try:
             self.url_parser.get_soup(url)
             text_response = "%s visited successfully!" % url
+            # Understand what type of page is.
+            if self.url_parser.is_article(url):
+                text_response += "\nThis page is an article."
+            else:
+                text_response += "\nThis page is a section."
+
         except Exception as ex:
             text_response = ex.args[0]
 
         # Update url in context.
-        return build_response(text_response, url, paragraph=0)
+        return build_response(text_response, url)
 
     def get_menu(self, url):
         text_response = self.url_parser.get_menu(url)
         # TODO get elements of menu, how many?
-        return build_response(text_response, url=url, paragraph=0)
+        return build_response(text_response, url=url)
 
     def get_info(self, url):
         text_response = self.url_parser.get_info(url)
-        return build_response(text_response, url=url, paragraph=0)
+        return build_response(text_response, url=url)
 
-    def read_page(self, url, paragraph):
+    def read_page(self, url, paragraph, article):
         # todo if page is article, read it. otherwise read main article titles available.
-        try:
-            text_response = self.url_parser.get_article(url, paragraph)
-            paragraph += 1
-        except IndexError as err:
-            text_response = "End of article."
-            paragraph = 0
-        return build_response(text_response, url=url, paragraph=paragraph)
+        if self.url_parser.is_article(url):
+            try:
+                text_response = self.url_parser.get_article(url, paragraph)
+                paragraph += 1
+            except IndexError as err:
+                text_response = "End of article."
+                paragraph = 0
+        else:
+            try:
+                text_response = self.url_parser.get_section(url, article)
+                article += 1
+            except IndexError as err:
+                text_response = "End of section."
+                article = 0
+        return build_response(text_response, url=url, paragraph=paragraph, article=article)
+
+    # def go_to_section(self, url):
+
+
+
+
--- url_parser.py
+++ url_parser.py
@@ -26,10 +26,43 @@ class UrlParser:
         # todo get info about the page.
         return soup.title.string
 
+    def is_article(self, url):
+        soup = self.get_soup(url)
+        # Count number of <article> tags in the page.
+        n_articles = soup.find_all(name="article")
+        return len(n_articles) < 8
+
+    def get_article(self, url, paragraph):
+        soup = self.get_soup(url)
+        # Find article div.
+        article_div = soup.find_all(name="div", attrs={'class': 'news__content'})[0]
+        # If paragraph is available, read it.
+        div_paragraphs = article_div.find_all('p')
+        string = "%s\n %d paragraph(s) left." % (div_paragraphs[paragraph].text, len(div_paragraphs) - paragraph)
+        return string
+
+    def get_section(self, url, article):
+        soup = self.get_soup(url)
+        # Get all articles in the section.
+        articles = soup.find_all(name="article")
+        text_response = "Article number %s/%d \n" % (str(article+1), len(articles))
+        text_response += articles[article].find(name="h2").text
+        print(text_response)
+        return text_response
+
     def get_menu(self, url):
         soup = self.get_soup(url)
-        # Get all the single text strings inside ul elements of class menu.
-        strings = [ul_menu.text for ul_menu in soup.findAll(name="ul", attrs={'class': 'menu'})]
+        # Get all the ul of class menu.
+        ul_lists = [ul_menu for ul_menu in soup.find_all(name="ul", attrs={'class': 'menu'})]
+        # Get all the li elements belonging to ul of class menu.
+        li_lists = [ul_list.find_all(name="li") for ul_list in ul_lists]
+        # Flatten list.
+        li_lists = [item for sublist in li_lists for item in sublist]
+        # Get anchors.
+        a_elements = [li_list.find_all(name="a") for li_list in li_lists]
+        # Remove duplicates in list.
+        a_elements = list(set(list(a_elements)))
+        # todo sends back tuple with text and url of anchor
         strings = [string.split('\n') for string in strings]
         # Flatten list while removing whitespaces.
         strings = [item.strip() for sublist in strings for item in sublist]
@@ -42,12 +75,6 @@ class UrlParser:
         print(joined_strings)
         return joined_strings
 
-    def get_article(self, url, paragraph):
-        # TODO recognize if page is a section or an article, behavior changes accordingly.
-        soup = self.get_soup(url)
-        # Find article div.
-        article_div = soup.findAll(name="div", attrs={'class': 'news__content'})[0]
-        # If paragraph is available, read it.
-        div_paragraphs = article_div.findAll('p')
-        string = "%s\n %d paragraph(s) left." % (div_paragraphs[paragraph].text, len(div_paragraphs) - paragraph)
-        return string
+    def go_to_section(self, url, name):
+        # Get menu.
+        menu = self.get_menu(url)
