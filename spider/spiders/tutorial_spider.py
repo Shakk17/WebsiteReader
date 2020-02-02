c22414f496f9279be985fa98742711d4988469ad
Alessandro Pina <pina.alessandro17@gmail.com>
2020-02-02 18:30:39 +0100

    Analysis of DB working.

--- spider/spiders/tutorial_spider.py
+++ spider/spiders/tutorial_spider.py
@@ -1,18 +1,20 @@
 import scrapy
 from scrapy.loader import ItemLoader
 from scrapy.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
 
 from spider.items import UrlItem
 
 
 class QuotesSpider(scrapy.Spider):
     name = "urls"
-    allowed_domains = ["toscrape.com"]
-    start_urls = ['http://quotes.toscrape.com/']
+    allowed_domains = ["polimi.it"]
+    start_urls = ['https://www.polimi.it/']
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.visited_links = []
+        self.link_processed = 0
 
     def parse(self, response):
         le = LinkExtractor()
@@ -23,10 +25,14 @@ class QuotesSpider(scrapy.Spider):
             url_item["text"] = link.text
             url_item["url_anchor"] = link.url
             url_item["found_in_page"] = response.url
+            self.link_processed += 1
 
             if link.url not in self.visited_links:
                 self.visited_links.append(link.url)
                 yield response.follow(link.url, callback=self.parse)
 
             yield url_item
+
+            if self.link_processed == 10000:
+                raise CloseSpider('bandwidth_exceeded')
 
