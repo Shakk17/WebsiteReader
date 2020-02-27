95495b810e6bab5fc341b34ef6425e5d79256b83
Alessandro Pina <Shakk17@users.noreply.github.com>
2020-02-27 12:28:11 +0100

    Delete analyze_scraping.py

--- analyze_scraping.py
+++
@@ -1,8 +0,0 @@
-from databases.database_handler import Database
-from datumbox_wrapper import DatumBox
-from helper import extract_search_forms
-import requests
-
-menu = Database().analyze_scraping("open")
-for el in menu:
-    print(el)
